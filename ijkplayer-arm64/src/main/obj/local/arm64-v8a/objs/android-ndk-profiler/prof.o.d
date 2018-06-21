01749dbbc807a95cc8fe3cd405944edefdf40dde
itchaosfun <1534249551@qq.com>
2018-06-21 12:47:14 +0800

    first commit

---
+++ ijkplayer-arm64/src/main/obj/local/arm64-v8a/objs/android-ndk-profiler/prof.o.d
@@ -0,0 +1,8 @@
+/home/ijkplayer-android/android/ijkplayer/ijkplayer-arm64/src/main/obj/local/arm64-v8a/objs/android-ndk-profiler/prof.o: \
+ /home/ijkplayer-android/android/ijkplayer/ijkplayer-arm64/src/main/jni/android-ndk-prof/prof.c \
+ /home/ijkplayer-android/android/ijkplayer/ijkplayer-arm64/src/main/jni/android-ndk-prof/prof.h \
+ /home/ndk/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/android/log.h
+
+/home/ijkplayer-android/android/ijkplayer/ijkplayer-arm64/src/main/jni/android-ndk-prof/prof.h:
+
+/home/ndk/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/android/log.h:
---
+++ ijkplayer-armv5/src/main/obj/local/armeabi/objs/android-ndk-profiler/prof.o.d
@@ -0,0 +1,8 @@
+/home/ijkplayer-android/android/ijkplayer/ijkplayer-armv5/src/main/obj/local/armeabi/objs/android-ndk-profiler/prof.o: \
+ /home/ijkplayer-android/android/ijkplayer/ijkplayer-armv5/src/main/jni/android-ndk-prof/prof.c \
+ /home/ijkplayer-android/android/ijkplayer/ijkplayer-armv5/src/main/jni/android-ndk-prof/prof.h \
+ /home/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/android/log.h
+
+/home/ijkplayer-android/android/ijkplayer/ijkplayer-armv5/src/main/jni/android-ndk-prof/prof.h:
+
+/home/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/android/log.h:
